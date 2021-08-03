0883b5d9f013a2612df68ca2036308a2a018d8dd
Kurachenko Vladislav <70369286+JOWISSA@users.noreply.github.com>
2021-08-03 22:30:38 +0300

    #1

---
+++ src/loader.js
@@ -0,0 +1,4 @@
+import React from "react"
+
+export default () => <div style={{display: 'flax', justifyContent: 'center', margin: '.5rem'}}>
+ <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>
